a0904656142fe77f3dc71a4dd771d1401cdd11f5
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-05-17 10:40:30 -0600

    add:gamebackend

--- models/usuario.js
+++ models/usuario.js
@@ -1,41 +1,7 @@
 const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  apellido_paterno: {
-    type: String,
-    required: true,
-  },
-  apellido_materno: {
-    type: String,
-    required: true,
-  },
-  edad: {
-    type: Number,
-    required: true,
-  },
-  genero: {
-    type: String,
-    required: true,
-  },
-  telefono: {
-    type: String,
-    required: true,
-  },
-  correo: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  grado_de_estudios: {
-    type: String,
-    required: true,
-  },
-  usuario: {
+  username: {
     type: String,
     required: true,
     unique: true,
@@ -44,16 +10,80 @@ const usuarioSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  imagenPerfil: String,
+  tipo: {
+    type: String,
+    required: true,
+  },
+  fecha_registro: {
+    type: Date,
+    default: Date.now,
+  },
+  datos_personales: {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    apellido_paterno: {
+      type: String,
+      required: true,
+    },
+    apellido_materno: {
+      type: String,
+      required: true,
+    },
+    edad: {
+      type: Number,
+      required: true,
+    },
+    genero: {
+      type: String,
+      required: true,
+    },
+    telefono: {
+      type: String,
+      required: true,
+    },
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    grado_de_estudios: {
+      type: String,
+      required: true,
+    },
+  },
   experiencia_en_lenguaje_de_programacion: {
     type: [String],
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  evaluaciones_realizadas: [
+    {
+      tema_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      preguntas_respondidas: [
+        {
+          pregunta_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+          },
+          respuesta_elegida: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      calificacion: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
 });
 
-const Usuario = mongoose.model('Usuario', usuarioSchema);
+const Usuario = mongoose.model('usuario', usuarioSchema);
 
 module.exports = Usuario;
