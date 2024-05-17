5a14044255372bc49e8e79e0b032de925ec2c61c
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-05-17 12:02:24 -0600

    add:gamebackend

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario');
 
 // Endpoint para obtener todos los usuarios
@@ -15,9 +16,16 @@ router.get('/usuarios', async (req, res) => {
 // Endpoint para crear un nuevo usuario
 router.post('/usuarios', async (req, res) => {
     try {
-        
+        // Cifrar el password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-        const nuevoUsuario = await Usuario.create(req.body);
+        // Crear el nuevo usuario con el password cifrado
+        const nuevoUsuario = new Usuario({
+            ...req.body,
+            password: hashedPassword
+        });
+
+        await nuevoUsuario.save();
         res.status(201).json(nuevoUsuario);
     } catch (error) {
         res.status(400).json({ message: error.message });
