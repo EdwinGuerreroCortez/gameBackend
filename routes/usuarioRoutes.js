0279ccba398576b9d50ae0ebbf927d24271b3771
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-06-26 23:43:03 -0600

    add:gamebackend

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -215,5 +215,21 @@ router.post('/usuarios/:id/evaluaciones', async (req, res) => {
       res.status(500).json({ message: error.message });
     }
 });
-
+router.put('/usuarios/:id/autorizar', async (req, res) => {
+    try {
+      const { autorizacion } = req.body;
+      const usuario = await Usuario.findById(req.params.id);
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+  
+      usuario.autorizacion = autorizacion;
+      await usuario.save();
+  
+      res.json({ message: 'Estado de autorización actualizado con éxito', autorizacion: usuario.autorizacion });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
 module.exports = router;
