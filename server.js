086fe91a530869742e960036cf9eb2fea9e22110
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-07-16 09:36:15 -0600

    add:gamebackend

--- server.js
+++ server.js
@@ -4,24 +4,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/usuarioRoutes');
 const imagenesRouter = require('./routes/crudImagenes');
-const imagenesRouterReact = require('./routes/imagenRoutes'); // Asegúrate de que el nombre del archivo esté correcto
+const imagenesRouterReact = require('./routes/imagenRoutes');
 const contactRoutes = require('./routes/contact');
 const misionVisionRoutes = require('./routes/misionVisionRoutes'); 
 const temaRoutes = require('./routes/temaRoutes');
 const evaluacionRoutes = require('./routes/evaluacionRoutes');
-const examenRoutes = require('./routes/examenRoutes'); // Importa las nuevas rutas de exámenes
-const cursosRoutes = require('./routes/cursosRoutes')
-
+const examenRoutes = require('./routes/examenRoutes');
+const cursosRoutes = require('./routes/cursosRoutes');
 
 const app = express(); 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Conectar a la base de datos MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a la base de datos MongoDB'))
   .catch(err => console.error('Error de conexión a MongoDB:', err));
 
@@ -33,9 +32,8 @@ app.use('/api', contactRoutes);
 app.use('/api', misionVisionRoutes);
 app.use('/api', temaRoutes);
 app.use('/api', evaluacionRoutes);
-app.use('/api', examenRoutes); // Añadir esta línea
+app.use('/api', examenRoutes);
 app.use('/api', cursosRoutes);
-
 
 // Iniciar el servidor
 app.listen(PORT, () => {
